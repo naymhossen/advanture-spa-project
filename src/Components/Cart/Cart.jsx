5aa5ae184087e2faa559b2098c6d918c5426590a
Naym Hossen <naym@Nayms-MBP.Dlink>
2023-09-13 12:09:07 +0600

    first commit

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+
+const Cart = () => {
+    return (
+        <div>
+            <h1 className="text-5xl underline animate-pulse font-bold text-center text-orange-500">My Cart</h1>
+        </div>
+    );
+};
+
+
+export default Cart;
